6ea2881ad824ae3b86db7d92fe36c7304c189229
v_wangyusen_dxm <v_wangyusen@duxiaoman.com>
2020-01-21 19:57:48 +0800

    save

--- zoro_amazon/zoro_amazon.py
+++ zoro_amazon/zoro_amazon.py
@@ -3,6 +3,7 @@ import time
 from lxml import etree
 import helper
 import sys
+import asyncio
 
 class zoro_amazon:
 
@@ -11,21 +12,15 @@ class zoro_amazon:
         # exit()
         self.config()
         # self.filein = open('./filein_10000.txt', 'r')  # filein_10000.txt
-        self.filein = open('./Untitled-1.txt', 'r')  # filein_10000.txt
         self.fileout = False
-        self.specalSeller = ['VPSupply']
-        # 阈值
-        self.writeLimit = 20
-        # 延时
-        self.sleepTime = 0.1
-        self.amazon_main()
+
+        self.time_str = time.strftime('%y%m%d_%H%M%S',time.localtime())
+        self.amazon_main(
+            self.inputData('./filein.txt')
+        )
 
     def __del__(self):
         pass
-        # if self.filein != False:
-        #     self.filein.close()
-        # if self.fileout != False:
-        #     self.fileout.close()
     
     def config(self):
         self.zoroSession = requests.Session()
@@ -69,44 +64,37 @@ class zoro_amazon:
         # tb:K26VQTG66CA8BAPPF0HX+s-6TST51X5TYE05BXPCCE1|1579336490948&t:1579336490948&adb:adblk_yes
         # tb:s-R3KDF4Q21GW4P18K12G8|1579336506593&t:1579336507469&adb:adblk_yes
 
-    def amazonGet(self, url):
-        headers = {
-            'user-agent': helper.get_user_agent(),
-        }
-        return self.aSession.get(url, headers=headers)
-
     def writePageLog(self, name, data):
         f = open(name,'wb')
         f.write(data)
         f.close()
 
     # 读
-    def inputData(self):
-        if self.filein == False:
-            print('open filein file')
-            self.filein = open('./filein.txt', 'r')
-        row = self.filein.readline()
+    def inputData(self, file = './filein.txt'):
+        filein = open(file, 'r')
+        row = filein.readline()
         flag = False
         while row.strip() != '':
             tmp = row.strip().split("\t")
             # if tmp[0] == 'G9178849':
             #     flag = True
             # if flag == False:
-            #     row = self.filein.readline()
+            #     row = filein.readline()
             #     continue
 
             # 列数为2是异常状态
             if len(tmp) == 2:
                 print(','.join(tmp),'error')
-                row = self.filein.readline()
+                row = filein.readline()
                 continue
             yield tmp
-            row = self.filein.readline()
-        self.filein.close()
+            row = filein.readline()
+        filein.close()
     
     # 写
-    def writeData(self, dataList, filepath = './fileout.txt'):
-        # print('open fileout file')
+    def writeData(self, dataList, filepath = ''):
+        if filepath == '':
+            filepath = './fileout_'+self.time_str+'.txt'
         fileout = open(filepath, 'a', encoding = 'utf-8')
         if type(dataList) == list and len(dataList) > 0:
             # 为列表
@@ -117,6 +105,10 @@ class zoro_amazon:
             else:
                 # 单行列表数据
                 fileout.write("\t".join(map(str, dataList)) + "\n")
+        else:
+            print('dataList empty.')
+            # dataList为空列表
+            return
         if type(dataList) == str:
             # 文本数据直接写
             fileout.write(dataList)
@@ -145,8 +137,9 @@ class zoro_amazon:
         while True:
             i += 1
             try:
-                response = self.amazonGet(
-                    'https://www.amazon.com/gp/offer-listing/{}/ref=olp_f_new?f_new=true'.format(sku))
+                response = self.aSession.get(
+                    'https://www.amazon.com/gp/offer-listing/{}/ref=olp_f_new?f_new=true'.format(sku),
+                    headers = {'user-agent': helper.get_user_agent()})
                 break
             except:
                 print('amazon() error,',sku)
@@ -156,12 +149,14 @@ class zoro_amazon:
 
         if response.status_code != 200:
             print('amazon status_code:', response.status_code,sku)
-            self.debug += 'a_get_err:' + str(response.status_code) + ';'
+            # self.debug += 'a_get_err:' + str(response.status_code) + ';'
+            self.writePageLog(sku + '.htm', response.content)
             time.sleep(5)
             return False, False
         else:
             if 'Try a different refinement' in response.text:
                 print('amazon get:Try a different refinement',sku)
+                self.writePageLog(sku + '.htm', response.content)
                 return False, False
 
         tree = etree.HTML(response.text)
@@ -212,7 +207,7 @@ class zoro_amazon:
         else:
             amazon_price = min(priceList)
             amazon_seller = sellerList[priceList.index(amazon_price)]
-            # if amazon_seller in self.specalSeller:
+            # if amazon_seller in self.specal_seller:
                 # self.debug += 'found specal seller;'
             # 返回最低价格和seller
             return amazon_price, amazon_seller
@@ -235,24 +230,44 @@ class zoro_amazon:
                 item['quant'] = 1
         return True
 
+    # self.write_list
+    # 写爬虫结果唯一入口，除了错误log
+    def add_data(self, data, filepath):
+        self.write_list.append(data)
+        if len(self.write_list) == self.write_limit:
+            self.writeData(self.write_list, filepath)
+            self.write_list = []
+
     # 主进程
-    def amazon_main(self):
-        ret = []
-        count = 0
-        for item in self.inputData():
-            time.sleep(self.sleepTime)
+    def amazon_main(self, generate):
+        self.specal_seller = ['VPSupply']
+        # 阈值
+        self.write_limit = 3
+        # 延时
+        self.sleep_time = 3
+        self.count = 0
+        self.write_list = []
+
+        loop = asyncio.get_event_loop()
+        tasks = [self.amazon_loop(generate, i) for i in range(2)]
+        loop.run_until_complete(asyncio.wait(tasks))
+        loop.close()
+    
+    async def amazon_loop(self, generate, number):
+        for item in generate:
+            time.sleep(self.sleep_time)
             self.debug = ''
-            # time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            count += 1
-            print(count, item)
+            self.count += 1
+            print(self.count, number, item)
             zoro_id = item[0]
             rid = item[1]
             amazon_id = item[2]
             # zoro_price, package = self.zoro(zoro_id)
             # zoro_new_price = zoro_price * package * 1.35 + 5
-            amazon_price, amazon_seller = self.amazon(amazon_id)
+            amazon_price, amazon_seller = await self.amazon(amazon_id)
             if amazon_seller == False:
-                self.writeData(item, './fileout_amazon_error.txt')
+                print('amazon_id:',amazon_id,' amazon_seller=False')
+                self.writeData(item, './amazon_error_'+self.time_str+'.txt')
                 continue
             # amazon价格为空 使用zoro价格
             '''
@@ -260,7 +275,7 @@ class zoro_amazon:
                 amazon_price = zoro_price
                 self.debug += 'a_price=None;'
             # 命中特殊seller
-            if amazon_seller in self.specalSeller:
+            if amazon_seller in self.specal_seller:
                 best_price = zoro_new_price
                 self.debug += 'mode=specal;'
             else:
@@ -277,13 +292,13 @@ class zoro_amazon:
                     best_price = zoro_price * 1.8 + 5
                     self.debug += 'mode=3;'
             '''
-            ret.append({
+            self.add_data({
                 'zoro_id' : zoro_id,
                 'rid' : rid,
                 'amazon_id' : amazon_id,
                 'amazon_price': round(amazon_price, 2),
                 'amazon_seller' : amazon_seller
-            })
+            },'./amazon_out_'+self.time_str+'.txt')
             # ret.append({
             #     'rid': rid,
 
@@ -305,14 +320,14 @@ class zoro_amazon:
             #     'amazon_seller':amazon_seller,
             #     'debug':self.debug
             # })
-            if len(ret) == self.writeLimit:
-            #     print('len(ret) == ' + str(self.writeLimit))
+            # if len(ret) == self.write_limit:
+            #     print('len(ret) == ' + str(self.write_limit))
             #     ret = self.zoroInstock(ret)
-                self.writeData(ret)
-                ret = []
+                # self.writeData(ret)
+                # ret = []
         # self.zoroInstock(ret)
-        self.writeData(ret)
-        ret = []
+        # self.writeData(ret)
+        # ret = []
 
 if __name__ == '__main__':
     zoro_amazon()
